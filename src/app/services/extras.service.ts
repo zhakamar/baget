08319185c301a05f8632f62bd703008d34cac341
asphix <zhakamar@yandex.ru>
2022-02-12 00:41:17 +0300

    optimize services

--- src/app/services/extras.service.ts
+++ src/app/services/extras.service.ts
@@ -1,19 +1,30 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {CoreService} from "./core.service";
-import {Observable} from "rxjs";
+import {Observable, ReplaySubject} from "rxjs";
 import {ExtrasRef} from "./extras.model";
+import {share} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExtrasService extends CoreService {
 
+  private extras$: Observable<ExtrasRef[]>;
+
   constructor(http: HttpClient) {
     super(http);
+
+    this.extras$ = super.get<ExtrasRef[]>('extras').pipe(
+      share({
+        connector: () => new ReplaySubject(1),
+        resetOnError: false,
+        resetOnComplete: false,
+        resetOnRefCountZero: false,
+      }),
+    );
   }
 
-  get ExtrasRef(): Observable<ExtrasRef[]> {
-    return super.get('extras');
-  }
+
+
 }
